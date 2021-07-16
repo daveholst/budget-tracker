4a4af12a9dce82bf2637583e27266d5d4b83dec8
Dave Holst <daveholst@gmail.com>
2021-07-16 18:42:31 +0800

    offline caching working correctly

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
 ];
 
 const CACHE_NAME = 'static-files-cache-v1';
-const API_CACHE_NAME = 'api-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install caching
 self.addEventListener('install', (e) => {
@@ -21,4 +21,40 @@ self.addEventListener('install', (e) => {
     })
   );
   self.skipWaiting();
+});
+
+self.addEventListener('fetch', function (evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) =>
+          fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) =>
+              // Network request failed, try to get it from the cache.
+              cache.match(evt.request)
+            )
+        )
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
