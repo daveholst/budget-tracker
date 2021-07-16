e7276223378662a05323d9b7705eb7a428e0839e
David Holst <daveholst@Davids-MacBook-Air.local>
2021-07-16 13:53:09 +0800

    setup linting

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/service-worker.js",
+  "/styles.css"
+]
+
+const CACHE_NAME = "static-files-cache-v1";
+const API_CACHE_NAME = "api-cache-v1";
+
+// install caching
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+
+      })
+  )
+} )
